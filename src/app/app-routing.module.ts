9b8efc0106fb141285e84490b8be95875f2de8a7
Samuel Santamaria <samuel.1037@hotmail.com>
2020-11-10 16:26:22 -0500

    ...

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ArticlesComponent } from './controllers/articles';
+import { ArticlesEComponent } from './controllers/articlesE';
 
 const routes: Routes = [
   { path: 'articles', component: ArticlesComponent },
+  { path: 'articles-e', component: ArticlesEComponent },
 ];
 
 @NgModule({
